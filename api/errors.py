024dc54ee1680965ff4ccc7896dd9159eeedea03
Leandro Damasio <leandro.damasio@boasnovasgestao.com.br>
2023-06-21 02:29:28 -0300

    Fix 404 error msg

--- api/errors.py
+++ api/errors.py
@@ -2,9 +2,13 @@ from flask import Blueprint, Response
 
 errors = Blueprint("errors", __name__)
 
+@errors.app_errorhandler(405)
+def server_error(error):
+    return Response(f"Method must be POST! {error}", status=405)
+
 @errors.app_errorhandler(404)
 def server_error(error):
-    return Response(f"Oops, got an error! {error}", status=404)
+    return Response(f"Inexistent route! {error}", status=404)
 
 @errors.app_errorhandler(Exception)
 def server_error(error):
